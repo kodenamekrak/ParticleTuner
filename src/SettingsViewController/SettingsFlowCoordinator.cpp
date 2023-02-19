6f7d6920365a894ff68fbfad54009164a7eaf504
kodenamekrak <kyelodge2005@gmail.com>
2023-02-19 19:47:28 +0000

    Tiny bit of bsml

--- src/SettingsViewController/SettingsFlowCoordinator.cpp
+++ src/SettingsViewController/SettingsFlowCoordinator.cpp
@@ -2,6 +2,9 @@
 #include "SettingsViewController/SettingsFlowCoordinator.hpp"
 
 #include "bsml/shared/BSML.hpp"
+#include "bsml/shared/Helpers/creation.hpp"
+#include "HMUI/ViewController_AnimationDirection.hpp"
+#include "HMUI/ViewController_AnimationType.hpp"
 
 DEFINE_TYPE(ParticleTuner, SettingsFlowCoordinator);
 
@@ -9,8 +12,17 @@ namespace ParticleTuner {
     SettingsViewController* settingsViewController;
 
     void SettingsFlowCoordinator::DidActivate(bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling)
-    {}
+    {
+        if (!firstActivation) return;
+        settingsViewController = BSML::Helpers::CreateViewController<ParticleTuner::SettingsViewController *>();
+
+        showBackButton = true;
+        ProvideInitialViewControllers(settingsViewController, nullptr, nullptr, nullptr, nullptr);
+        SetTitle("Particle Tuner", HMUI::ViewController::AnimationType::In);
+    }
 
     void SettingsFlowCoordinator::BackButtonWasPressed(HMUI::ViewController* topViewController)
-    {}
+    {
+        this->parentFlowCoordinator->DismissFlowCoordinator(this, HMUI::ViewController::AnimationDirection::Horizontal, nullptr, false);
+    }
 }
